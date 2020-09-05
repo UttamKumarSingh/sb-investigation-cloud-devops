749f48560dc6d1f57919068eddc99f1d1830bcdc
Uttam <Uttam@host.docker.internal>
2020-09-05 11:41:08 +0900

    First upload

---
+++ src/test/java/com/uks/investigation/InvestigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uks.investigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestigationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
